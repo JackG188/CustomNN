a965104a13ba50aa602e522572f2783f92e90266
JackG188 <jackmgraham@hotmail.co.uk>
2014-04-02 13:47:39 +0100

    Added object class to hold input connections for each neuron

---
+++ CustomNN/src/com/jackgraham/customNN/ParsedNet.java
@@ -0,0 +1,23 @@
+package com.jackgraham.customNN;
+
+import java.util.ArrayList;
+
+public class ParsedNet {
+
+	ArrayList<Integer> inputs = new ArrayList<Integer>();
+	
+	public ParsedNet()
+	{
+		
+	}
+
+	public ArrayList<Integer> getInputs() {
+		return inputs;
+	}
+
+	public void setInputs(ArrayList<Integer> inputs) {
+		this.inputs = inputs;
+	}
+	
+	
+}
